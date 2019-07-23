70ea3cbac0328d61890d22e59f857998fd262c54
huskyareyouscared <1710632585@qq.com>
2019-07-23 13:33:44 +0800

    新增编辑器主界面，tool-package初始化

--- lib/husky-crazy-edit/lib/main.vue
+++ lib/husky-crazy-edit/lib/main.vue
@@ -1,14 +1,7 @@
 <!--  -->
 <template>
-  <div class='card-container'>
-    <div class="card-header" :class="selfTheme" :style="{height:headerHight,lineHeight:headerHight}">
-      <span v-show="titleDecoration" class="card-header-small"></span>
-      <span v-show="titleDecoration" class="card-header-middle"></span>
-      <span class="card-title">{{title}}</span>
-      <span v-show="titleDecoration" class="card-header-middle"></span>
-      <span v-show="titleDecoration" class="card-header-small"></span>
-    </div>
-    <slot></slot>
+  <div class='main' contenteditable="true">
+    <div  class="tool-package"></div>
   </div>
 </template>
 
@@ -16,34 +9,12 @@
 export default {
   props: {
     // 设置卡片标题
-    title: {
-      type: String,
-      default: '可以设置标题'
-    },
-    // 卡片标题装饰物是否显示
-    titleDecoration: {
-      type: Boolean,
-      default: true
-    },
-    // 卡片的主题色
-    cardTheme: {
-      type: String,
-      default: 'blue'
-    },
-    // 卡片的高度
-    headerHight: {
-      type: String,
-      default: '46px'
-    }
   },
   // import引入的组件需要注入到对象中才能使用
   components: {},
   data () {
     // 这里存放数据
     return {
-      selfTheme: {
-        [this.cardTheme]: true
-      }
     }
   },
   // 监听属性 类似于data概念
@@ -61,60 +32,18 @@ export default {
 }
 </script>
 <style  scoped>
-  .card-container{
-    width: 100%;
-    height: auto;
-    padding: 0px;
-    margin: 0px;
-  }
-  .card-header{
-    width: 100%;
-    /* height:46px; */
-    text-align: center;
-    border-radius:6px 6px 0px 0px;
-    /* line-height: 46px; */
-  }
-  .card-header-small::before{
-    margin-left: 0.5vw;
-    margin-right: 0.5vw;
-    content: "";
-    color: #fff;
-    width: 0.5vw;
-    height: 0.5vw;
-    background: #fff;
-    display: inline-block;
-    transform: rotateZ(45deg) scale(0.7);
-  }
-  .card-header-middle::before{
-    margin-left: 0.6vw;
-    margin-right: 0.6vw;
-    content: "";
-    color: #fff;
-    width: 0.5vw;
-    height: 0.5vw;
-    background: #fff;
-    display: inline-block;
-    transform: rotateZ(45deg) scale(0.9);
-  }
-  .card-title{
-    color: #fff;
-    font-size: 22px;
-  }
-
-  /* 主题色  theme */
-  .orange{
-     background:linear-gradient(-78deg,rgba(251,119,81,1),rgba(244,159,66,1));
-  }
-  .blue{
-    background:linear-gradient(-51deg,rgba(94,223,240,0.96),rgba(11,184,248,0.96));
-  }
-  .green{
-     background:linear-gradient(-78deg,rgba(57,233,178,1),rgba(67,233,214,1));
-  }
-  .purple{
-     background:linear-gradient(-78deg,rgba(109,86,251,1),rgba(155,86,251,1));
-  }
-  .pink{
-     background:linear-gradient(-78deg,rgba(249,77,144,0.96),rgba(249,74,75,0.96));
-  }
+.main{
+  width: 100%;
+  height: 100vh;
+  border: 1px dashed #ccc;
+}
+.tool-package{
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  right: 50px;
+  top: 50px;
+  border: #ccc dashed 2px;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import huskyCrazyEdit from '../lib/husky-crazy-edit'
 Vue.config.productionTip = false
-
+Vue.use(huskyCrazyEdit)
 new Vue({
   router,
   store,
